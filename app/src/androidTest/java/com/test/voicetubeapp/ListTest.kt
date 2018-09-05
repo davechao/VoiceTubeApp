95e0f72701bd3010684516fc0a0ecef72621ad15
Dave Chao <dave.chao@isuncloud.com>
2018-09-05 12:01:03 +0800

    refactor UI Test

--- app/src/androidTest/java/com/test/voicetubeapp/ListTest.kt
+++ app/src/androidTest/java/com/test/voicetubeapp/ListTest.kt
@@ -6,15 +6,11 @@ import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import android.view.View
-import android.view.ViewGroup
 import org.junit.Test
 import org.junit.runner.RunWith
 import com.test.voicetubeapp.ui.main.MainActivity
-import org.hamcrest.Description
-import org.hamcrest.Matcher
+import com.test.voicetubeapp.utils.TestUtils.childAtPosition
 import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 
 @RunWith(AndroidJUnit4::class)
@@ -42,21 +38,4 @@ class ListTest {
         bottomNavigationItemView.perform(click())
     }
 
-    companion object {
-        fun childAtPosition(parentMatcher: Matcher<View>, position: Int): Matcher<View> {
-            return object: TypeSafeMatcher<View>() {
-                override fun describeTo(description: Description) {
-                    description.appendText("Child at position $position in parent ")
-                    parentMatcher.describeTo(description)
-                }
-
-                override fun matchesSafely(view: View): Boolean {
-                    val parent = view.parent
-                    return (parent is ViewGroup
-                            && parentMatcher.matches(parent)
-                            && view == parent.getChildAt(position))
-                }
-            }
-        }
-    }
 }
--- app/src/androidTest/java/com/test/voicetubeapp/SettingTest.kt
+++ app/src/androidTest/java/com/test/voicetubeapp/SettingTest.kt
@@ -6,13 +6,9 @@ import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import android.view.View
-import android.view.ViewGroup
 import com.test.voicetubeapp.ui.main.MainActivity
-import org.hamcrest.Description
-import org.hamcrest.Matcher
+import com.test.voicetubeapp.utils.TestUtils.childAtPosition
 import org.hamcrest.Matchers
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,31 +29,13 @@ class SettingTest {
     private fun showSettingView() {
         val bottomNavigationItemView = Espresso.onView(
                 Matchers.allOf(ViewMatchers.withId(R.id.navigation_setting),
-                        ListTest.childAtPosition(
-                                ListTest.childAtPosition(
+                        childAtPosition(
+                                childAtPosition(
                                         ViewMatchers.withId(R.id.bottomNavigation),
                                         0),
                                 1),
                         ViewMatchers.isDisplayed()))
         bottomNavigationItemView.perform(ViewActions.click())
-    }
-
-    companion object {
-        fun childAtPosition(parentMatcher: Matcher<View>, position: Int): Matcher<View> {
-            return object: TypeSafeMatcher<View>() {
-                override fun describeTo(description: Description) {
-                    description.appendText("Child at position $position in parent ")
-                    parentMatcher.describeTo(description)
-                }
-
-                override fun matchesSafely(view: View): Boolean {
-                    val parent = view.parent
-                    return (parent is ViewGroup
-                            && parentMatcher.matches(parent)
-                            && view == parent.getChildAt(position))
-                }
-            }
-        }
     }
 
 }
